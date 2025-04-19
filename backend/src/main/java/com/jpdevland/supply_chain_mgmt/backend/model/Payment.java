0d174784cbfcdd3212838e0685ebde0e1a95f24e
Babum2107 <babum210704@gmail.com>
2025-04-19 13:40:24 +0530

    Test

--- backend/src/main/java/com/jpdevland/supply_chain_mgmt/backend/model/Payment.java
+++ backend/src/main/java/com/jpdevland/supply_chain_mgmt/backend/model/Payment.java
@@ -1,4 +1,34 @@
 package com.jpdevland.supply_chain_mgmt.backend.model;
 
-public class Payment {
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name="payments",schema = "public")
+public class Payment
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    @Column(nullable = false)
+    private BigDecimal order_id;
+
+    @Column(nullable = false, length = 50)
+    private String payment_gateway;
+
+    @Column(nullable = false,length = 50)
+    private String payment_status;
+
+    @Column(nullable = false,length = 255)
+    private String transaction_id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false,columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime payment_date=OffsetDateTime.now();
+
 }
